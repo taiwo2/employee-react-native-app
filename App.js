bb19849f0211d41f117c936bfbe860a7a1620057
taiwo2 <adetonat@ymail.com>
2024-05-04 23:43:23 +0100

    Remove file

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants'
+import Home  from './screens/Home'
+import CreateEmployee from './screens/CreateEmployee'
+import Profile from './screens/Profile';
 
-export default function App() {
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+// import {createStore} from 'redux'
+// import {Provider} from 'react-redux'
+// import {reducer} from './reducers/reducer'
+
+// const store  = createStore(reducer)
+
+
+const Stack = createStackNavigator();
+
+const myOptions = {
+  title:"My Sweet Home",
+  headerTintColor:"white",
+  headerStyle:{
+    backgroundColor:"#006aff"
+  }
+}
+function App() {
   return (
+  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     
+      <Stack.Navigator>
+        <Stack.Screen 
+        name="Home" 
+        component={Home}
+         options={myOptions} />
+        <Stack.Screen 
+         name="Create"
+         component={CreateEmployee}
+         options={{...myOptions,title:"Create Employee"}} 
+         />
+        <Stack.Screen
+         name="Profile"
+         component={Profile}
+         options={{...myOptions,title:"Profile"}} 
+          />
+     </Stack.Navigator>
+  
     </View>
+
   );
+}
+
+
+export default ()=>{
+  return (
+    // <Provider store={store}>
+    
+    // </Provider>
+    <NavigationContainer>
+    <App />
+  </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#e0e0e0',
   },
 });
